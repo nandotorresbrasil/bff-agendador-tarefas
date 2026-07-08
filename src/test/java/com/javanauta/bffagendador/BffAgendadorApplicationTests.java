29f3ec5375f25caa50749dc3074aa904f2518814
Fernando Torres <nandotorresbrasil@gmail.com>
2026-07-08 17:21:36 -0300

    adicionado esqueleto do projeto

---
+++ src/test/java/com/javanauta/bffagendador/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
